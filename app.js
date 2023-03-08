a4b93eba5f43f60c803c73f488ecb2134dbaef00
ela_lisondra <emrlisondra@gmail.com>
2023-03-08 13:27:51 +0000

    CH10 Create an object called post to console log both title and body

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const contents = [];
+const post = [];
 
 app.set('view engine', 'ejs');
 
@@ -37,7 +37,11 @@ app.get("/compose", function(req, res){
 })
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  const post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  }
+  console.log(post);
   
 })
 
