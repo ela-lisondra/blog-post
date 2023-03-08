3bfde49f576b7f55bf1cc04a37b79a82379fa72c
ela_lisondra <emrlisondra@gmail.com>
2023-03-08 11:59:23 +0000

    CH7 Render Compose page and create input textbox

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.get("/contact", function(req, res){
   
 });
 
+app.get("/compose", function(req, res){
+  res.render("compose" );
+})
+
+// app.post("/compose", function(req, res){
+  
+// })
+
 
 
 
